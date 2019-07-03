b49088c4a8f7924057d00c3434188a5199abab06
sh06y <47583852+sh06y@users.noreply.github.com>
2019-07-03 20:52:11 +0800

    Add files via upload

--- money.py
+++ money.py
@@ -2,12 +2,13 @@ import os
 import time
 
 count=1
-def a():
+def get():
     print("正在刷新...")
-    os.system('adb shell input swipe 550 426 560 1700')
+    os.system('adb shell input tap 105 1890')
+    time.sleep(0.5)
     print("刷新成功")
 
-    print("点开第",count,"条新闻")
+    print("点开第",count+1,"条新闻")
     os.system('adb shell input tap 300 600')
     time.sleep(1)
 
@@ -18,6 +19,16 @@ def a():
     print("返回主页面")
     os.system('adb shell input tap 51 130')
 
-#循环20遍
-for count in range(1,20):
-    a()
+print("无限循环 or 循环n次？[1|2]")
+a = input("请输入1或2：")
+
+if a == '1':
+    print("----无限循环模式启动----")
+    while 1:
+        get()
+elif a == '2':
+    n = input("请输入遍数：")
+    print("----循环",n,"遍----")
+    for count in range(0, int(n)):
+        get()
+
